c94bd1143a19c3b28c6bae352f6bd90d6217c57d
Andres Toledo <49215828+AndresMToledo@users.noreply.github.com>
2020-06-10 18:07:34 -0400

    sql-tags finished
    tag table and book-tag table finished

--- sql-core/script-final.sql
+++ sql-core/script-final.sql
@@ -19,6 +19,23 @@ create table readifined.book(
 	cover_img bytea,
 	book bytea,
 	constraint book_id_pk primary key (id),
-	constraint book_author_fk foreign key (author) references readifined.person(id)
+	constraint book_author_fk foreign key (author) references readifined.person(id) on delete cascade on update cascade
+);
+
+drop table if exists readifined.tag cascade;
+create table readifined.tag(
+	id serial,
+	tag_name varchar(100) unique not null,
+	constraint tag_id_pk primary key (id)
+);
+
+drop table if exists readifined.book_tags cascade;
+create table readifined.book_tags(
+	id serial,
+	book_id integer,
+	tag_id integer,
+	constraint book_tags_id_pk primary key (id),
+	constraint book_tags_book_id_fk foreign key (book_id) references readifined.book(id) on delete cascade on update cascade,
+	constraint book_tags_tag_id_fk foreign key (tag_id) references readifined.tag(id) on delete cascade on update cascade
 );
 
