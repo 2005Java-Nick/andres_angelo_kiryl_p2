256f77764a0f336a6633a5d79a54bf060d7e3a95
kiryl_p <kiryl.pashkouski@gmail.com>
2020-06-10 16:46:14 -0400

    added DBeaver project

---
+++ Scripts/script_create_tables.sql
@@ -0,0 +1,14 @@
+/*******************************************************
+ * READIFY - Version 0.1;
+ * Script: script_create_tables.sql;
+ * Desciption: creates tables ;
+ * Author: @
+*******************************************************/
+/*******************************************************
+	CREATE TABLES
+*******************************************************/
+drop schema if exists readify ;
+create schema readify;
+/******************************************************/
+drop table if exists;
+create table
